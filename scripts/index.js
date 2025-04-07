70f8152daa1632e34b0f736c4422eeab8165bc43
Артем Рябцев <artem@riabtsev.ru>
2025-04-07 22:06:59 +0300

    fix: добавить js функционал

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,104 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
+const profilePopup = document.querySelector('.popup_type_edit');
+const cardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileFormElement = document.querySelector('.popup__form[name="edit-profile"]');
+const nameInput = profileFormElement.querySelector('.popup__input_type_name');
+const jobInput = profileFormElement.querySelector('.popup__input_type_description');
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const cardFormElement = document.querySelector('.popup__form[name="new-place"]');
+const cardNameInput = cardFormElement.querySelector('.popup__input_type_card-name');
+const cardLinkInput = cardFormElement.querySelector('.popup__input_type_url');
+const placesList = document.querySelector('.places__list');
+const popupImage = document.querySelector('.popup__image');
+const popupCaption = document.querySelector('.popup__caption');
 
-// @todo: DOM узлы
+profilePopup.classList.add("popup_is-animated");
+cardPopup.classList.add("popup_is-animated");
+imagePopup.classList.add("popup_is-animated");
 
-// @todo: Функция создания карточки
+function createCard(data) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const likeButton = cardElement.querySelector('.card__like-button');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('card__like-button_is-active');
+  });
+
+  deleteButton.addEventListener('click', () => {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener('click', () => {
+    popupImage.src = data.link;
+    popupImage.alt = data.name;
+    popupCaption.textContent = data.name;
+    openModal(imagePopup);
+  });
+
+  return cardElement;
+}
+
+function clearForm(form) {
+  form.reset();
+}
+
+function renderInitialCards() {
+  initialCards.forEach((card) => {
+    const cardElement = createCard(card);
+    placesList.append(cardElement);
+  });
+}
+
+function openModal(popup) {
+  popup.classList.add('popup_is-opened');
+}
+
+function closeModal(popup) {
+  popup.classList.remove('popup_is-opened');
+}
+
+document.querySelectorAll('.popup__close').forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closeModal(popup));
+});
+
+profileEditButton.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+  openModal(profilePopup);
+});
+
+profileFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal(profilePopup);
+});
+
+document.querySelector('.profile__add-button').addEventListener('click', () => {
+  clearForm(cardFormElement);
+  openModal(cardPopup);
+});
+
+cardFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const newCard = {
+    name: cardNameInput.value,
+    link: cardLinkInput.value
+  };
+  placesList.prepend(createCard(newCard));
+  closeModal(cardPopup);
+});
+
+renderInitialCards();
